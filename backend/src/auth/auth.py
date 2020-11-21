4ba3cb6d2cab8785a32156ec9d7be7a41a19452c
AniaJezierska <candyrockstore5@gmail.com>
2020-11-21 16:41:26 +0100

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'dev-i7v2mtub.eu.auth0.com'
 ALGORITHMS = ['RS256']    
-API_AUDIENCE = 'drinks'
+API_AUDIENCE = 'https://AniaJezierska.github.io'
 
 ## AuthError Exception      
 '''
